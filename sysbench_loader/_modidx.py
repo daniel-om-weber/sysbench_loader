79e66ee7932735901921903f6f37071836de66f5
Daniel Weber <danielusweber@gmail.com>
2024-06-10 11:34:52 +0200

    added dev requirement

--- sysbench_loader/_modidx.py
+++ sysbench_loader/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://daniel-om-weber.github.io',
                 'git_url': 'https://github.com/daniel-om-weber/sysbench_loader',
                 'lib_path': 'sysbench_loader'},
-  'syms': {'sysbench_loader.core': {'sysbench_loader.core.foo': ('core.html#foo', 'sysbench_loader/core.py')}}}
+  'syms': { 'sysbench_loader.core': { 'sysbench_loader.core.to_hdf5': ('core.html#to_hdf5', 'sysbench_loader/core.py'),
+                                      'sysbench_loader.core.write_array': ('core.html#write_array', 'sysbench_loader/core.py'),
+                                      'sysbench_loader.core.write_dataset': ('core.html#write_dataset', 'sysbench_loader/core.py')},
+            'sysbench_loader.datasets.wh': {}}}
