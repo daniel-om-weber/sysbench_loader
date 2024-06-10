a52f1e4799cb2b4f5c0989bf4e099be28acafb84
Daniel Weber <danielusweber@gmail.com>
2024-06-10 11:41:22 +0200

    out of sync

--- sysbench_loader/_modidx.py
+++ sysbench_loader/_modidx.py
@@ -7,5 +7,4 @@ d = { 'settings': { 'branch': 'main',
                 'lib_path': 'sysbench_loader'},
   'syms': { 'sysbench_loader.core': { 'sysbench_loader.core.to_hdf5': ('core.html#to_hdf5', 'sysbench_loader/core.py'),
                                       'sysbench_loader.core.write_array': ('core.html#write_array', 'sysbench_loader/core.py'),
-                                      'sysbench_loader.core.write_dataset': ('core.html#write_dataset', 'sysbench_loader/core.py')},
-            'sysbench_loader.datasets.wh': {}}}
+                                      'sysbench_loader.core.write_dataset': ('core.html#write_dataset', 'sysbench_loader/core.py')}}}
